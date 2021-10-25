1928bce4ab6e4d3affae223bb3988d4b416be5be
coro-to <79664185+coro-to@users.noreply.github.com>
2021-10-24 23:57:08 -0500

    entrenador

---
+++ Sprint_4_Grupo7/JS/entrenador.js
@@ -0,0 +1,36 @@
+/*entrenador*/
+document.querySelector("#editar").onclick = function () {
+    new bootstrap.Toast(document.querySelector('#basicToast')).show();
+}
+document.querySelector("#editar1").onclick = function () {
+    new bootstrap.Toast(document.querySelector('#basicToast')).show();
+}
+document.querySelector("#editar2").onclick = function () {
+    new bootstrap.Toast(document.querySelector('#basicToast')).show();
+}
+document.querySelector("#editar3").onclick = function () {
+    new bootstrap.Toast(document.querySelector('#basicToast')).show();
+}
+document.querySelector("#eliminar").onclick = function () {
+    new bootstrap.Toast(document.querySelector('#basicToast2')).show();
+}
+document.querySelector("#eliminar1").onclick = function () {
+    new bootstrap.Toast(document.querySelector('#basicToast2')).show();
+}
+document.querySelector("#eliminar2").onclick = function () {
+    new bootstrap.Toast(document.querySelector('#basicToast2')).show();
+}
+document.querySelector("#eliminar3").onclick = function () {
+    new bootstrap.Toast(document.querySelector('#basicToast2')).show();
+}
+document.querySelector("#Eagregar").onclick = function () {
+    new bootstrap.Toast(document.querySelector('#basicToast3')).show();
+}
+/*agregar entrenador*/
+document.querySelector("#basicToastBtn").onclick = function () {
+    new bootstrap.Toast(document.querySelector('#basicToast')).show();
+}
+/*guardar entrenador*/
+document.querySelector("#basicToastBtn").onclick = function () {
+    new bootstrap.Toast(document.querySelector('#basicToast')).show();
+}
